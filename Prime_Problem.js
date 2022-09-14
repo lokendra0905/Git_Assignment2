e18715a7d49b0750e870c4d198137fbfdcbe1bde
Mohit Negi <address>
2022-09-14 16:17:45 +0530

    code error is resolved

--- Prime_Problem.js
+++ Prime_Problem.js
@@ -1,11 +1,13 @@
 // checl if 9 is prime number or not
+let n=9
 let count=0;
-let arr=[1,2,3,4,5,6,7,8,9,]
-for (i=1;i<=arr.length;i++){
-    if(arr[arr.length-1]%arr[arr.length-1]==0&&arr[arr.length-1]%1==0)
-    count++;
-}if(count==2){
-    console.log("Prime");
-}else {
-    console.log("Not Prime");
+for(let i=1;i<=n;i++){
+	if(n%i==0){
+		count++
+}
+    }
+    if(count==2){
+console.log("Prime");
+}else{
+	console.log("Not prime");
 }
