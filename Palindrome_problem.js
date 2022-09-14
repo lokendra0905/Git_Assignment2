728ed5e1ab52c5d9aad2686c5f0acccd138392d9
Mohit Negi <address>
2022-09-14 07:38:26 +0530

    code is done

--- Palindrome_problem.js
+++ Palindrome_problem.js
@@ -1,1 +1,10 @@
 // check if "naman" is palindrome or not 
+let str="naman";
+let bag=""
+for (let i=str.length-1;i>-1;i--){
+    bag=bag+str[i]
+}if(bag==str){
+    console.log("Palindrome")
+}else {
+    console.log("Not Palindrome")
+}
--- Prime_Problem.js
+++ Prime_Problem.js
@@ -1,1 +1,11 @@
 // checl if 9 is prime number or not
+let count=0;
+let arr=[1,2,3,4,5,6,7,8,9,]
+for (i=1;i<=arr.length;i++){
+    if(arr[arr.length-1]%arr[arr.length-1]==0&&arr[arr.length-1]%1==0)
+    count++;
+}if(count==2){
+    console.log("Prime");
+}else {
+    console.log("Not Prime");
+}
